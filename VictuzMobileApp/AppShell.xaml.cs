6961602b655b0009f550904a62d14958c4b94a91
Stef Konings <2302771konings@zuyd.nl>
2025-01-17 10:30:37 +0100

    Add project files.

---
+++ VictuzMobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VictuzMobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VictuzMobileApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VictuzMobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
