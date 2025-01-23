5584e51084050d6659701ad24bea0e6e4396fdab
Stef Konings <2302771konings@zuyd.nl>
2025-01-23 14:19:24 +0100

    Opzet Database

---
+++ VictuzMobileApp/MVVM/Data/Constants.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VictuzMobileApp.MVVM.Model;
+
+namespace VictuzMobileApp.MVVM.Data
+{
+    public class Constants
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public Constants(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            try
+            {
+                // Tabellen maken
+
+                _database.CreateTableAsync<User>().Wait();
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating tables: {ex.Message}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+                throw;
+            }
+        }
+    }
