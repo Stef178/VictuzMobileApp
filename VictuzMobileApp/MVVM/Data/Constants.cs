a2cd086a735ef68df1b30c9192ed73e3497da13e
Stef Konings <2302771konings@zuyd.nl>
2025-01-23 15:00:35 +0100

    CRUD mogelijkheden toegoevoegd

--- VictuzMobileApp/MVVM/Data/Constants.cs
+++ VictuzMobileApp/MVVM/Data/Constants.cs
@@ -33,6 +33,31 @@ namespace VictuzMobileApp.MVVM.Data
                 Console.WriteLine($"StackTrace: {ex.StackTrace}");
                 throw;
             }
+             
+        }
+        public Task<List<T>> GetAllAsync<T>() where T : new()
+        {
+            return _database.Table<T>().ToListAsync();
+        }
+
+        public Task<int> AddAsync<T>(T item) where T : new()
+        {
+            return _database.InsertAsync(item);
+        }
+
+        public Task<int> UpdateAsync<T>(T item) where T : new()
+        {
+            return _database.UpdateAsync(item);
+        }
+
+        public Task<int> DeleteAsync<T>(T item) where T : new()
+        {
+            return _database.DeleteAsync(item);
+        }
+
+        public Task<int> AddAllAsync<T>(IEnumerable<T> items) where T : new()
+        {
+            return _database.InsertAllAsync(items);
         }
     }
 }
