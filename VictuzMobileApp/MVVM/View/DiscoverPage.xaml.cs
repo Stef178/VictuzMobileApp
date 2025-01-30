b7bb4205c584caf14a3d7f16f0f47a035bb5995b
Kasper Bakker <2305440bakker@zuyd.nl>
2025-01-30 13:01:24 +0100

    zoekbalk toegevoegd aan de discovery page

--- VictuzMobileApp/MVVM/View/DiscoverPage.xaml.cs
+++ VictuzMobileApp/MVVM/View/DiscoverPage.xaml.cs
@@ -1,10 +1,13 @@
 using VictuzMobileApp.MVVM.Model;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace VictuzMobileApp.MVVM.View
 {
 	public partial class DiscoverPage : ContentPage
 	{
-		public List<Activity> AllEvents { get; set; }
+		public ObservableCollection<Activity> FilteredEvents { get; set; } = new ObservableCollection<Activity>();
+		private List<Activity> AllEvents { get; set; } = new List<Activity>();
 
 		public DiscoverPage()
 		{
@@ -17,15 +20,25 @@ namespace VictuzMobileApp.MVVM.View
 			try
 			{
 				var activities = await App.Database.GetAllAsync<Activity>();
-
 				AllEvents = activities.OrderBy(a => a.StartTime).ToList();
-
+				FilteredEvents = new ObservableCollection<Activity>(AllEvents);
 				BindingContext = this;
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine($"Error loading events: {ex.Message}");
 				await DisplayAlert("Error", "Er is een fout opgetreden bij het laden van de evenementen.", "OK");
+			}
+		}
+
+		private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+		{
+			string searchText = e.NewTextValue.ToLower();
+			FilteredEvents.Clear();
+
+			foreach (var activity in AllEvents.Where(a => a.Name.ToLower().Contains(searchText)))
+			{
+				FilteredEvents.Add(activity);
 			}
 		}
 	}
