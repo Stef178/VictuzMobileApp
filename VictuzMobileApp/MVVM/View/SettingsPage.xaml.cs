b63d3db2c100d8938897a8e94e9472638f5fdc9b
Stef Konings <2302771konings@zuyd.nl>
2025-01-30 14:14:21 +0100

    Blue light toggle filter toegeoegd en funcitoneel gemaakt binnen gehele app

--- VictuzMobileApp/MVVM/View/SettingsPage.xaml.cs
+++ VictuzMobileApp/MVVM/View/SettingsPage.xaml.cs
@@ -2,8 +2,28 @@ namespace VictuzMobileApp.MVVM.View;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
-	{
-		InitializeComponent();
-	}
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+
+    // Methode die de knop activeert om het filter toe te voegen
+    private void OnNoBlueLightButtonClicked(object sender, EventArgs e)
+    {
+        bool isBlueLightActivated = Preferences.Get("NoBlueLight", false);
+        isBlueLightActivated = !isBlueLightActivated;
+
+        // Sla de voorkeur op
+        Preferences.Set("NoBlueLight", isBlueLightActivated);
+
+        // Wijzig de achtergrondkleur voor de gehele app
+        if (isBlueLightActivated)
+        {
+            Application.Current.Resources["AppBackgroundColor"] = Color.FromRgb(255, 255, 180); // Geel
+        }
+        else
+        {
+            Application.Current.Resources["AppBackgroundColor"] = Colors.White; // Normale achtergrondkleur
+        }
+    }
 }
