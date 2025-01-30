3e842325064aa8468cc4930d5c3405e2e94f34e2
Stef Konings <2302771konings@zuyd.nl>
2025-01-30 10:16:55 +0100

    Database fixes uitgevoerd en layout actviteit aanmaken

--- VictuzMobileApp/MVVM/Model/Activity.cs
+++ VictuzMobileApp/MVVM/Model/Activity.cs
@@ -19,12 +19,12 @@ namespace VictuzMobileApp.MVVM.Model
         [NotNull]
         public DateTime EndTime { get; set; }
         [NotNull]
-		public int MaxParticipants { get; set; }
+        public int MaxParticipants { get; set; }
 		[Ignore]
 		public Organisor? Organisor { get; set; }
         [Ignore]
         public ICollection<Participant>? Participants { get; set; }
-
+        [NotNull]
         public string PhotoPath { get; set; }
     }
 }
