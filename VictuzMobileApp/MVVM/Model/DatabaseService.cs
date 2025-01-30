8dca842916aa708e70cac1a7d596802c270f4bc6
itsfinns <finnemans12@gmail.com>
2025-01-30 13:29:13 +0100

    Fotos en onderschrift worden nu in database opgeslagen.

---
+++ VictuzMobileApp/MVVM/Model/DatabaseService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VictuzMobileApp.MVVM.View
+{
+    public class PhotoModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+        public string Caption { get; set; }
+    }
+
+    public class DatabaseService
+    {
+        private SQLiteConnection _connection;
+
+        // Pad naar de database (in de lokale appmap)
+        public DatabaseService()
+        {
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "photos.db3");
+            _connection = new SQLiteConnection(databasePath);
+            _connection.CreateTable<PhotoModel>(); // Maak de tabel aan als deze nog niet bestaat
+        }
+
+        // Voeg een foto toe aan de database
+        public void AddPhoto(PhotoModel photo)
+        {
+            _connection.Insert(photo);
+        }
+
+        // Haal alle foto's op uit de database
+        public List<PhotoModel> GetAllPhotos()
+        {
+            return _connection.Table<PhotoModel>().ToList();
+        }
+    }
+}
+
--- VictuzMobileApp/MVVM/View/CommunityPage.xaml.cs
+++ VictuzMobileApp/MVVM/View/CommunityPage.xaml.cs
@@ -1,54 +1,63 @@
 using Microsoft.Maui.Media;
 using System.Collections.ObjectModel;
+using Microsoft.Maui.Media;
+using System.Collections.ObjectModel;
 
-namespace VictuzMobileApp.MVVM.View;
-
-public partial class CommunityPage : ContentPage
+namespace VictuzMobileApp.MVVM.View
 {
-    public class PhotoModel
+    public partial class CommunityPage : ContentPage
     {
-        public string ImagePath { get; set; }
-        public string Caption { get; set; }
-    }
-
-    private ObservableCollection<PhotoModel> _photoList = new ObservableCollection<PhotoModel>();
-
-    public CommunityPage()
-    {
-        InitializeComponent();
-
-        // Hardcoded voorbeeldfoto's
-        _photoList.Add(new PhotoModel { ImagePath = "example1.png", Caption = "Eerste voorbeeldfoto" });
-        _photoList.Add(new PhotoModel { ImagePath = "example2.png", Caption = "Tweede voorbeeldfoto" });
+        private ObservableCollection<PhotoModel> _photoList = new ObservableCollection<PhotoModel>();
 
         
-        PhotoCollectionView.ItemsSource = _photoList;
-    }
+        private DatabaseService _dbService;
 
-    private async void OnAddPhotoClicked(object sender, EventArgs e)
-    {
-        var photo = await MediaPicker.PickPhotoAsync();
-        if (photo != null)
+        public CommunityPage()
         {
-            // onderschrift
-            string caption = await DisplayPromptAsync("Foto Toevoegen", "Onderschrift toevoegen:");
+            InitializeComponent();
 
             
-            if (string.IsNullOrWhiteSpace(caption))
+            _dbService = new DatabaseService();
+
+            
+            var savedPhotos = _dbService.GetAllPhotos();
+
+            
+            foreach (var photo in savedPhotos)
             {
-                caption = "Geen onderschrift opgegeven";
+                _photoList.Add(photo);
             }
 
-            _photoList.Add(new PhotoModel
-            {
-                ImagePath = photo.FullPath,
-                Caption = caption
-            });
-
-            // verversen
-            PhotoCollectionView.ItemsSource = null;
+            
             PhotoCollectionView.ItemsSource = _photoList;
         }
-    }
 
+        private async void OnAddPhotoClicked(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.PickPhotoAsync();
+            if (photo != null)
+            {
+                
+                string caption = await DisplayPromptAsync("Foto Toevoegen", "Onderschrift toevoegen:");
+
+                if (string.IsNullOrWhiteSpace(caption))
+                {
+                    caption = "Geen onderschrift opgegeven";
+                }
+
+                var newPhoto = new PhotoModel
+                {
+                    ImagePath = photo.FullPath,
+                    Caption = caption
+                };
+
+                _photoList.Add(newPhoto);
+
+                _dbService.AddPhoto(newPhoto);
+
+                PhotoCollectionView.ItemsSource = null;
+                PhotoCollectionView.ItemsSource = _photoList;
+            }
+        }
+    }
 }
